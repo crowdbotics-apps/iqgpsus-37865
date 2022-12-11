9b36bbe4370e332c6f1d4be6016e5f5c2187fc08
Monica Izarraras <info@spyshopone.net>
2022-12-10 18:16:43 -0800

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "iqgpsus_37865",
   url: "https://iqgpsus_37865.botics.co",
   api: "https://iqgpsus_37865.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
